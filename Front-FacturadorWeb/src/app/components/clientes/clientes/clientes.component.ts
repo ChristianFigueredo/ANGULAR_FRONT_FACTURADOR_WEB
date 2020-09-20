9f61fd5c05028afeba08290f645dafdf93c52016
Christian Figueredo <figueredoflorez@gmail.com>
2020-09-20 10:59:51 -0500

    update Frontend FacturadorWeb

--- Front-FacturadorWeb/src/app/components/clientes/clientes/clientes.component.ts
+++ Front-FacturadorWeb/src/app/components/clientes/clientes/clientes.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ClientesService } from '../../../services/clientes.service';
-import { Router } from '@angular/router';
-import { ResponseClienteModel } from '../../../Models/responseClienteModel';
-import { TipoDocumentoService } from '../../../services/tipo-documento.service';
-import { TipoDocumento } from '../../../Models/tipoDocumento';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-clientes',
@@ -16,6 +10,7 @@ export class ClientesComponent implements OnInit {
 
 
   constructor() {
+
   }
 
   ngOnInit() {
